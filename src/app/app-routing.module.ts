78109a1dc2e8167059c1001083f33b418200c80d
Mauricio Alvarado <mauriciorianoa@gmail.com>
2024-11-05 13:14:02 -0500

    feat: Ajuste de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, provideRouter } from '@angular/router';
 import { RegistroActasComponent } from './modules/registro-actas/registro-actas.component';
+import {getSingleSpaExtraProviders} from "single-spa-angular";
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { APP_BASE_HREF } from '@angular/common';
 
 const routes: Routes = [
   {
-    path:"registro-actas",
+    path:"registrar",
     component: RegistroActasComponent
   }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    provideRouter(routes),
+    { provide: APP_BASE_HREF, useValue: '/actas/' },
+    getSingleSpaExtraProviders(),
+    provideHttpClient(withFetch()),
+  ],
 })
 export class AppRoutingModule { }
